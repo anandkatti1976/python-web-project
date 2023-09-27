c876758a0fc2af9a8cdaf5d72f48150f90d67aee
Anand Katti <anandkatti@gmail.com>
2023-09-27 17:14:49 +0530

    Added css changes and render changes

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+JOBS = [ 
+    {
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bangalore",
+        "salary": "Rs 10,00,000"
+    },
+    {
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Mumbai",
+
+    },
+    {
+        "id": 3,
+        "title": "Full Stack Developer",
+        "location": "Bangalore",
+        "salary": "Rs 12,00,000"
+    },
+    {
+        "id": 4,
+        "title": "Back End Developer",
+        "location": "New York",
+        "salary": "$ 120,000"
+    }
+]
 
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html",
+                           jobs=JOBS,
+                           company_name="Jovian")
+
+@app.route("/api/jobs")
+def get_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
